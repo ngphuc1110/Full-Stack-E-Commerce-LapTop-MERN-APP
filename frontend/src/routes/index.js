d8a52d8482829f4d84f6b1b2eda37eb8981f536b
ngphuc1110 <phuc.11102001@gmail.com>
2024-11-19 23:43:15 +0700

    routes

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,62 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import Header from '../components/Header'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import SignUp from '../pages/SignUp'
+import ForgotPassword from '../pages/ForgotPassword'
+import AdminPanel from '../pages/AdminPanel'
+import AllUsers from '../pages/AllUsers'
+import AllProducts from '../pages/AllProducts'
+import BrandProduct from '../pages/BrandProduct'
+import ProductDetails from '../pages/ProductDetails'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "",
+                element: <Home />
+            },
+            {
+                path: "login",
+                element: <Login />
+            },
+            {
+                path: "forgot-password",
+                element: <ForgotPassword />
+            },
+            {
+                path: "sign-up",
+                element: <SignUp />
+            },
+            {
+                path: "brand/:brandName",
+                element: <BrandProduct />
+            },
+            {
+                path: "product/:id",
+                element: <ProductDetails />
+            },
+            {
+                path: "admin-panel",
+                element: <AdminPanel />,
+                children: [
+                    {
+                        path: "all-users",
+                        element: <AllUsers />
+                    },
+                    {
+                        path: "all-products",
+                        element: <AllProducts />
+                    },
+                ]
+            },
+
+        ]
+    }
+])
+
+export default router
