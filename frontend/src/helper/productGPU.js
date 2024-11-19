3d3a71b50c5d257de0dd878dd9d3652d42240c1f
ngphuc1110 <phuc.11102001@gmail.com>
2024-11-20 02:07:06 +0700

    Update

--- frontend/src/helper/productGPU.js
+++ frontend/src/helper/productGPU.js
@@ -5,9 +5,10 @@ const productGPU = [
     { id: 4, label: "GeForce RTX™ 2050 4GB", value: "RTX-2050" },
     { id: 5, label: "GeForce RTX™ 3050Ti 4GB", value: "RTX-3050Ti" },
     { id: 6, label: "GeForce RTX™ 3050 6GB", value: "RTX-3050" },
-    { id: 7, label: "GeForce RTX™ 4050 6GB", value: "RTX-4050" },
-    { id: 8, label: "GeForce RTX™ 4060 8GB ", value: "RTX-4060" },
-    { id: 9, label: "GeForce RTX™ 4080 8GB", value: "RTX-4080" },
+    { id: 7, label: "GeForce RTX™ 3060 6GB", value: "RTX-3060" },
+    { id: 8, label: "GeForce RTX™ 4050 6GB", value: "RTX-4050" },
+    { id: 9, label: "GeForce RTX™ 4060 8GB ", value: "RTX-4060" },
+    { id: 10, label: "GeForce RTX™ 4080 8GB", value: "RTX-4080" },
 
 ]
 
--- frontend/src/helper/productScreen.js
+++ frontend/src/helper/productScreen.js
@@ -1,7 +1,7 @@
 const productScreen = [
     { id: 1, label: "15,6 inch", value: "15,6" },
-    { id: 2, label: "16 inch IPS", value: "16 IPS" },
-    { id: 2, label: "-", value: "" },
+    { id: 2, label: "15,6 inch IPS 144Hz", value: "15,6 inch IPS 144Hz" },
+    { id: 3, label: "16 inch IPS", value: "16 IPS" },
     { id: 4, label: "-", value: "" },
     { id: 5, label: "-", value: "" },
 ]
