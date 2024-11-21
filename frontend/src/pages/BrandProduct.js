33d633734c639d10d2506eac9ca239fcfda32fc9
ngphuc1110 <phuc.11102001@gmail.com>
2024-11-21 13:08:59 +0700

    update

--- frontend/src/pages/BrandProduct.js
+++ frontend/src/pages/BrandProduct.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import productBrand from '../helper/productBrand'
 import productChipSet from '../helper/productChipSet'
 import productGPU from '../helper/productGPU'
@@ -11,8 +11,30 @@ const BrandProduct = () => {
     const params = useParams()
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(false)
-    const [selectBrand, setSelectBrand] = useState({})
+    const navigate = useNavigate()
+
+    const location = useLocation()
+    const urlSearch = new URLSearchParams(location.search)
+    const urlCategoryListinArray = urlSearch.getAll("brand")
+    const urlCategoryListinArray2 = urlSearch.getAll("cpu")
+    // const urlCategoryListinArray = [
+    //     ...urlCategoryListinArray1,
+    //     ...urlCategoryListinArray2
+    // ]
+    const rulCategoryListObject = {}
+    urlCategoryListinArray.forEach(el => {
+        rulCategoryListObject[el] = true
+    })
+    console.log("rulCategoryListObject", rulCategoryListObject)
+    console.log("urlCategoryListinArray", urlCategoryListinArray)
+
+
+
+    const [selectBrand, setSelectBrand] = useState(rulCategoryListObject)
     const [filterCategoryList, setFilterCategoryList] = useState([])
+
+    const [sortBy, setSortBy] = useState("")
+
 
     const fetchData = async () => {
         const response = await fetch(SummaryApi.filterProduct.url, {
@@ -27,7 +49,6 @@ const BrandProduct = () => {
 
         const dataResponse = await response.json()
         setData(dataResponse?.data || [])
-        console.log("dataResponse", dataResponse)
 
     }
     const handleSelectBrand = (e) => {
@@ -56,25 +77,48 @@ const BrandProduct = () => {
 
         setFilterCategoryList(arrayOfBrand)
 
+        //format for url change when change on the chexbox
+        const urlFormat = arrayOfBrand.map((el, index) => {
+            if ((arrayOfBrand.length - 1) === index) {
+                return `brand=${el}`
+            }
+            return `brand=${el}&&`
+        })
+        navigate("/category-product/?" + urlFormat.join(""))
+        //category-product/?brand=acer&&brand=asus&&cpu=5
     }, [selectBrand])
 
+    const handleOnChangeSortBy = (e) => {
+        const { value } = e.target
+        setSortBy(value)
+        if (value === 'asc') {
+            setData(preve => preve.sort((a, b) => a.sellingPrice - b.sellingPrice))
+        }
+        if (value === 'dsc') {
+            setData(preve => preve.sort((a, b) => b.sellingPrice - a.sellingPrice))
+        }
+    }
+
+    useEffect(() => {
+
+    }, [sortBy])
 
     return (
         <div className='container mx-auto p-4 '>
             <div className='hidden lg:grid grid-cols-[250px,1fr]'>
                 {/* left side */}
-                <div className='bg-white p-2 min-h-[calc(100vh-120px)] overflow-y-scroll'>
+                <div className='bg-white p-2 min-h-[calc(100vh-120px)] overflow-y-scroll max-h-[calc(100vh-120px)]'>
 
                     {/* Sort By */}
                     <div className=' '>
                         <h3 className='text-lg uppercase font-medium text-slate-500 border-b bp-2 border-slate-500'>Sort by</h3>
                         <form className='text-sm flex flex-col gap-2 py-2'>
                             <div className='flex items-center gap-3'>
-                                <input type='radio' name='sortBy' />
+                                <input type='radio' name='sortBy' checked={sortBy === 'asc'} value={"asc"} onChange={handleOnChangeSortBy} />
                                 <label>Price - Low to High</label>
                             </div>
                             <div className='flex items-center gap-3'>
-                                <input type='radio' name='sortBy' />
+                                <input type='radio' name='sortBy' checked={sortBy === 'dsc'} value={"dsc"} onChange={handleOnChangeSortBy} />
                                 <label>Price - High to Low</label>
                             </div>
                         </form>
@@ -134,16 +178,16 @@ const BrandProduct = () => {
                     </div>
                 </div>
                 {/* Display Product        */}
-                <div className=''>
-                    {
-                        data.length !== 0 && !loading && (
-                            <VerticalProductSearch data={data} loading={loading} />
+                <div className='px-10'>
+                    <p className='font-medium text-slate-400 text-lg my-2 '>Search Results: {data.length}</p>
+                    <div className='min-h-[calc(100vh-120px)] overflow-y-scroll max-h-[calc(100vh-120px)]'>
+                        {
+                            data.length !== 0 && (
+                                <VerticalProductSearch data={data} loading={loading} />
 
-                        )
-                        // params?.brandName && (
-                        //     <OtherProductDisplay brandName={params?.brandName} heading="Similar Products" />
-                        // )
-                    }
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </div>
